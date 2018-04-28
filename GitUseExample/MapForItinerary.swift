ff624d4668ff25665360e141e837971d6674582b
Deger Yentur <yentur.deger@gmail.com>
2018-04-28 13:53:16 +0200

    Added a new Class

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Deger Ugur Yentur on 28.04.18.
+//  Copyright © 2018 MacSoftySoft. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
